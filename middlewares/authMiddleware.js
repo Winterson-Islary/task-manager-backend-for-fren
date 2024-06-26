86e9ec74c637f32d38f56c7bbeea6745b39a800d
Winterson <u19cse1028@cit.ac.in>
2024-06-26 23:59:32 +0530

    made changes to authMiddleware to check for refresh-tokens

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,12 +1,29 @@
 const jwt = require("jsonwebtoken");
+const genToken = require("../helpers/genTokens");
 
 const authMiddleware = async (req, res, next) => {
 	try {
-		const token = req.headers.authorization?.split(" ")[1];
-		const verified = token ? await verifyToken(token) : false;
-		if (!verified) {
+		// const token = req.headers.authorization?.split(" ")[1];
+		const token = req.cookies.ACCESS_TOKEN;
+		const refresh_token = req.cookies.REFRESH_TOKEN;
+		const verified_access = token ? await verifyToken(token) : false;
+		const verified_refresh = refresh_token
+			? await verifyToken(refresh_token)
+			: false;
+		if (!verified_access && !verified_refresh) {
 			return res.status(403).json({
 				message: "unauthorized",
+			});
+		}
+		if (!verified_access && verified_refresh) {
+			const data = {
+				id: verified_refresh.id,
+				name: verified_refresh.name,
+				email: verified_refresh.email,
+			};
+			res.cookie(ACCESS_TOKEN, genToken.generateAccessToken(data), {
+				httpOnly: true,
+				secure: true,
 			});
 		}
 	} catch (error) {
@@ -20,7 +37,10 @@ const authMiddleware = async (req, res, next) => {
 async function verifyToken(token) {
 	try {
 		const verification = await jwt.verify(token, process.env.JWT_SECRET);
-		return true;
+		if (!verification) {
+			return false;
+		}
+		return verification;
 	} catch (error) {
 		return false;
 	}
