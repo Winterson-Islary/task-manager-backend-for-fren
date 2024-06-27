f51d12793a78ed11427cda857af5850b982f69ca
Winterson <u19cse1028@cit.ac.in>
2024-06-27 21:41:50 +0530

    added auth middleware to task routes

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const taskController = require("../controller/tasksController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
-router.get("/getTask", taskController.getTasks);
-router.post("/createTask", taskController.createTask);
-router.put("/updateTask", taskController.updateTask);
-router.delete("/deleteTask", taskController.deleteTask);
+router.get("/getTask", authMiddleware, taskController.getTasks);
+router.post("/createTask", authMiddleware, taskController.createTask);
+router.put("/updateTask", authMiddleware, taskController.updateTask);
+router.delete("/deleteTask", authMiddleware, taskController.deleteTask);
 
 module.exports = router;
